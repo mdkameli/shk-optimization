7c0df22b0a5e3009eaabf9d1513e7234e963fd11
Osel-hue <58938283+Osel-hue@users.noreply.github.com>
2020-02-14 16:59:18 +0100

    accomodate extra decision variable
    add different transmission rates for different channel gains and noise energies. Add more time and energy equations. Increase the size of matrices and vectors

--- optimization_consequtive.m
+++ optimization_consequtive.m
@@ -3,69 +3,80 @@ close all; clear all; clc;
 %% Define Variables
 K = 10;                                 % Number of tasks
 B = 5;                                  % Bandwidth (Mhz)
-sig = 1e-9;                             % Noise Energy (w)
-H_k = 1e-6;                             % Signal Energy
+sig = 1e-9;                             % Noise Energy (w) for transmission btw node and relay
+sig_rel = 0.75*1e-9;                    % Noise Energy (w) for transmission btw relay and edge  
+sig_edg = 1.2*1e-9;                     % Noise Energy (w) for transmission btw node and edge 
+H_k = 1e-6;                             % Signal Energy (channel gain between the node and relay(2nd device))
 d_k = unifrnd(300,500,K,1);             % data size (uniformly distributed btw 300-500 KB)
-w_k = 30;                               % CPU cycl for each bit in task (cycl/bit)
+w_k = 30;                               % CPU cycle for each bit in task (cycl/bit)
 Tmax = 4;                               % service threshold
 floc_k = unifrnd(0.1,0.5,K,1);          % local CPU cycl frequency (uniform dist. btw 0.1-0.5 Gcyc/s)
-fedg_k = 2;                             % edge CPU cycl frequency (Gcyc/s)
-fclo_k = 4;                             % Cloud CPU cycl frequency (Gcyc/s)
+fedg_k = 2;                             % edge CPU cycle frequency (Gcyc/s)
+frel_k = 1;                             % assume 2nd device(relay) CPU cycle frequency (Gcyc/s)
 k1 = 1e-27;                             % Effective switched capacitance
-ptra_k = 0.1;                           % Idle transmission power (w)
+ptra_k = 0.1;                           % Idle transmission power (w) (of local node)
 pcir_k = unifrnd(0.001,0.01,K,1);       % Idle circuit power (uniform dist. btw 0.001-0.01 w)
-R_EC = 5;                               % Transmission rate btw relay and cloud (MB/s)
+ptra_rel_k = 0.2;                       % Transmission power (w) of relay
+H_rel_k = 1.5*1e-6;                     % channel gain between the relay and edge
+H_edg_k = 1.1*1e-6;                     % channel gain between the node and edge
+% B_rel = 8;                            % Relay bandwidth
+
 %% Initial Vector Computation
-d_k = 8*1000.*d_k;                                              %% KB to bit
-C_k = w_k.*d_k;                                                 % CPU required to complete task k (cycle)
-B = B*1e6;                                                      %% Mhz to hz
-r_k = B*log2(1+(ptra_k*H_k/sig));                               % Transmission rate btw device and relay (bit/s)
-floc_k = 1e9.*floc_k;                                           %% Gcyc/s to cyc/s
-Tloc_k = C_k./floc_k;                                           % Local transmission time
-Eloc_k = (k1*floc_k.^2).*C_k;                                    % Local energy cunsumption
-fedg_k = 1e9*fedg_k;                                            %% Gcyc/s to cyc/s
-Tedg_k = d_k./r_k + C_k./fedg_k;                                % Relay transmission time
-Eedg_k = ptra_k*(d_k./r_k) + pcir_k.*(C_k./fedg_k);             % Realy energy consumption
-fclo_k = 1e9.*fclo_k;                                           %% Gcyc/s to cyc/s
-R_EC = 1e6*8*R_EC;                                              %% MB/s to bit/s
-Tclo_k = d_k./r_k + d_k./R_EC + C_k./fclo_k;                    % Cloud transmission time
-Eclo_k = ptra_k*(d_k./r_k) + pcir_k.*(d_k./R_EC + C_k./fclo_k); % Cloud energy consumption
+d_k = 8*1000.*d_k;                                                                          %% KB to bit
+C_k = w_k.*d_k;                                                                             % CPU required to complete task k (cycle)
+B = B*1e6;                                                                                  %% Mhz to hz
+% B_rel = B_rel*1e6;                                                                        %% Mhz to hz
+r_k = B*log2(1+(ptra_k*H_k/sig));                                                           % Transmission rate btw device and relay (bit/s)
+r_rel_k = B*log2(1+(ptra_rel_k*H_rel_k/sig_rel));                                           % Transmission rate btw relay and edge (bit/s)
+r_edg_k = B*log2(1+(ptra_k*H_edg_k/sig_edg));                                               % Transmission rate btw device and edge (bit/s)
+floc_k = 1e9.*floc_k;                                                                       %% Gcyc/s to cyc/s
+Tloc_k = C_k./floc_k;                                                                       % Local transmission time
+Eloc_k = (k1*floc_k.^2).*C_k;                                                               % Local energy cunsumption
+frel_k = 1e9.*frel_k;                                                                       %% Gcyc/s to cyc/s
+Td2d_k = d_k./r_k + C_k./frel_k;                                                            % Transmission time when data send from local node --> relay
+Ed2d_k = ptra_k*(d_k./r_k) + pcir_k.*(C_k./frel_k);                                         % Energy consumption when execution done at relay
+fedg_k = 1e9*fedg_k;                                                                        %% Gcyc/s to cyc/s
+Trel_edg_k = d_k./r_k + d_k./r_rel_k + C_k./fedg_k;                                         % Transmission time when data send from local node --> relay --> edge
+Erel_edg_k = ptra_k*(d_k./r_k) + ptra_rel_k*(d_k./r_rel_k) + pcir_k.*(C_k./fedg_k);         % Energy consumption when data send from local node  --> relay --> edge
+Tedg_k = d_k./r_edg_k + C_k./fedg_k;                                                        % Transmission time when data send directly from local node --> edge
+Eedg_k = ptra_k*(d_k./r_edg_k) + pcir_k.*(C_k./fedg_k);                                     % Energy consumption when data send directly from local node --> edge
+
 %% Initial Vector re-ordering
-b0 = [Eloc_k.';Eedg_k.';Eclo_k.'];
+b0 = [Eloc_k.';Ed2d_k.'; Erel_edg_k.'; Eedg_k.'];
 b0 = b0(:);
 b0 = [b0 ; pcir_k ; 0];
-b1= [zeros(1,3*K-3), Tloc_k(K), Tedg_k(K), Tclo_k(K), zeros(1,K-1), 1, 0].';
-b2 = [Tloc_k.' ;Tedg_k.' ; Eclo_k.'];
+b1= [zeros(1,4*K-4), Tloc_k(K), Td2d_k(K), Trel_edg_k(K), Tedg_k(K), zeros(1,K-1), 1, 0].';
+b2 = [Tloc_k.' ;Td2d_k.' ;Trel_edg_k.' ;Tedg_k.'];
 b2 = b2(:);
 b2 = [b2 ; ones(K,1)];
 %% 
-e=@(j) [zeros(j-1,1);1;zeros(4*K+1-j,1)];
-e2=@(j) [zeros(j-1,1);1;zeros(4*K-j,1)];
-bkp=@(k) (e(3*k-2)+e(3*k-1)+e(3*k));
-bj=@(j) (e2(3*j-2)+e2(3*j-1)+e2(3*j)+e2(3*K+j));
+e=@(j) [zeros(j-1,1);1;zeros(5*K+1-j,1)];
+e2=@(j) [zeros(j-1,1);1;zeros(5*K-j,1)];
+bkp=@(k) (e(4*k-3)+e(4*k-2)+e(4*k-1)+e(4*k));
+bj=@(j) (e2(4*j-3)+e2(4*j-2)+e2(4*j-1)+e2(4*j)+e2(4*K+j));
 
 %%% handel definition
-a0=zeros(4*K+1,4*K+1);
-a00=zeros(4*K,4*K);
+a0=zeros(5*K+1,5*K+1);
+a00=zeros(5*K,5*K);
 %a1=@(j) (-1/2)*(b2.'*diag(bj(j))).';
-%a2=@(k) (1/2)*e(3*K+k);
+%a2=@(k) (1/2)*e(4*K+k);
 
 %% Problem matrix definition
 M0=[a0, (1/2)*b0; (1/2)*b0.', 0];
 Mj=@(j) [diag(e(j)), (-1/2)*e(j); (-1/2)*e(j).', 0];
 Mkp=@(k) [a0, (1/2)*bkp(k); (1/2)*bkp(k).', 0];
 Mkd=[a0, (1/2)*b1; (1/2)*b1.', 0];
-Mkr=@(k) [a0, (1/2)*e(3*K+k); (1/2)*e(3*K+k).', 0];
-Mkrj=@(j,k) [a00, (-1/2)*(b2.'*diag(bj(j))).', (1/2)*e2(3*K+k);...
-    (-1/2)*(b2.'*diag(bj(j))), 0, 0; (1/2)*e2(3*K+k).', 0, 0];
+Mkr=@(k) [a0, (1/2)*e(4*K+k); (1/2)*e(4*K+k).', 0];
+Mkrj=@(j,k) [a00, (-1/2)*(b2.'*diag(bj(j))).', (1/2)*e2(4*K+k);...
+    (-1/2)*(b2.'*diag(bj(j))), 0, 0; (1/2)*e2(4*K+k).', 0, 0];
 %% Optimization Formulation
 cvx_solver sedumi
 cvx_begin sdp
-    variable G(4*K+2, 4*K+2) semidefinite
+    variable G(5*K+2, 5*K+2) semidefinite
     minimize (trace(M0*G))
     subject to
-%          G == semidefinite(4*K+2)
-        for i=1:3*K
+%          G == semidefinite(5*K+2)
+        for i=1:4*K
             trace(Mj(i)*G) == 0
         end
 %%%%%%%%        
@@ -81,14 +92,14 @@ cvx_begin sdp
 %%%%%%%% Subjected regarding node-1 as a start point
         trace(Mkr(1)*G) == 0
 %%%%%%%%
-        G(4*K+1,4*K+1) == 1
-        G(4*K+1,4*K+2) == 1
-        G(4*K+2,4*K+1) == 1
-        G(4*K+2,4*K+2) == 1
+        G(5*K+1,5*K+1) == 1
+        G(5*K+1,5*K+2) == 1
+        G(5*K+2,5*K+1) == 1
+        G(5*K+2,5*K+2) == 1
 cvx_end
 G = full(G);
 x=sqrt(diag(G));
-plot(x(1:3*K),'*');
+plot(x(1:4*K),'*');
 %% Result
 fprintf("Minimum Energy = %f",trace(M0*G));
 
