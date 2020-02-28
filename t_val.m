80afe14652d060a788d20996d9c8e88b79d5fa38
Osel-hue <58938283+Osel-hue@users.noreply.github.com>
2020-02-28 18:44:50 +0100

    get value for student t-distributions v degrees of freedom, 95% CI

---
+++ t_val.m
@@ -0,0 +1,9 @@
+function [t_star] = 
+%function to obtain value for t-distributions with v degrees of freedom for
+%two-sided critical regions and 95% confidence interval
+positive_side = 1-0.05/2;   
+negative_side = 0.05/2;
+upp = tinv(positive_side,v);
+low = tinv(negative_side,v);
+end
+
