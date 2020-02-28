bd52c7d5a9fc7afa0dcdd95e7082d85434c67091
MD kameli <39493047+mdkameli@users.noreply.github.com>
2020-02-28 19:35:51 +0100

    Update t_val.m

--- t_val.m
+++ t_val.m
@@ -1,4 +1,4 @@
-function [t_star] = 
+function [upp] = t_val(v)
 %function to obtain value for t-distributions with v degrees of freedom for
 %two-sided critical regions and 95% confidence interval
 positive_side = 1-0.05/2;   
